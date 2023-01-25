17f1d7eba4f107468e3832e0cd795d3f9cc0a238
MonicaDeBona <debona.monica@outlook.it>
2023-01-25 15:44:55 +0100

    Create bootstrap table

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotels</title>
+
+    <!-- Importing bootstrap v5.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
     <main>
         <?php
-
         $hotels = [
             [
                 'name' => 'Hotel Belvedere',
@@ -49,20 +51,34 @@
                 'distance_to_center' => 50
             ],
         ];
-
         ?>
 
-        <pre>
-            <?php
-            foreach ($hotels as $key => $value) {
-                foreach ($value as $key => $element) {
-                    echo '
-                    ';
-                    echo "$key: $element";
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $key => $hotel) {
+                    echo "
+                    <tr>
+                    <td>{$hotel['name']}</td>
+                    <td>{$hotel['description']}</td>
+                    <td>{$hotel['parking']}</td>
+                    <td>{$hotel['vote']}</td>
+                    <td>{$hotel['distance_to_center']}</td>
+                    </tr>
+                    ";
                 }
-            }
-            ?>
-        </pre>
+                ?>
+            </tbody>
+        </table>
     </main>
 </body>
 
